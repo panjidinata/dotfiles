2dc4e51bab7dc0b1544582e766ec0b89aaf0db91
panjidinata <panjidinata88@gmail.com>
2021-10-25 09:35:39 +0800

    edit username

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -6,7 +6,7 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Panji Dinata"
+(setq user-full-name "panjidinata"
       user-mail-address "panjidinata@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
